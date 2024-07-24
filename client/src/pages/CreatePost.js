a47610c450ed9bb19a0125dce7a0d7217c8d12b0
MoeOnGit <malrobiay04@gmail.com>
2024-07-24 15:46:36 -0600

    Overall updates

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -20,7 +20,7 @@ export default function CreatePost(){
         data.set('content', content);
         data.set('file', files[0]);
         ev.preventDefault();
-        const response = await fetch('https://blog-app-ashen-theta.vercel.app/post', {
+        const response = await fetch('http://localhost:4000/post', {
             method: 'POST',
             body: data,
             credentials: 'include',
--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -8,7 +8,7 @@ export default function PostPage(){
     const {userInfo} = useContext(UserContext);
     const {id} = useParams();
     useEffect(() => {
-        fetch(`https://blog-app-ashen-theta.vercel.app/post/${id}`)
+        fetch(`http://localhost:4000/post/${id}`)
         .then(response => {
             response.json().then(postInfo => {
                 setPostInfo(postInfo);
@@ -35,7 +35,7 @@ export default function PostPage(){
                 </div>
             )}
             <div className="image">
-                <img src={`https://blog-app-ashen-theta.vercel.app/${postInfo.cover}`} alt=""/>
+                <img src={`http://localhost:4000/${postInfo.cover}`} alt=""/>
             </div>
             <div className="content" dangerouslySetInnerHTML={{__html:postInfo.content}} />
         </div>
