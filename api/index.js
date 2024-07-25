3fa4106b9e8b34386db935084c0dc6004381959e
MoeOnGit <malrobiay04@gmail.com>
2024-07-25 15:22:36 -0600

    mongo

--- api/index.js
+++ api/index.js
@@ -21,7 +21,12 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 
 
 
-mongoose.connect('mongodb+srv://blog:EspC1s25mqYEKsJm@cluster0.8tvggus.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect('mongodb+srv://blog:EspC1s25mqYEKsJm@cluster0.8tvggus.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true
+});
+
 
 app.post('/register', async (req,res) => {
   const {username,password} = req.body;
