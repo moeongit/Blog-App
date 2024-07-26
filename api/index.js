759772481919b0e568a07361b6743d016f643bc7
MoeOnGit <malrobiay04@gmail.com>
2024-07-26 01:41:06 -0600

    y

--- api/index.js
+++ api/index.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const User = require('./models/User');
 const Post = require('./models/Post');
 const bcrypt = require('bcryptjs');
+const app = express();
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const multer = require('multer');
-const path = require('path'); // Import the path module
+const uploadMiddleware = multer({ dest: 'uploads/' });
 const fs = require('fs');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdjasjfkgjkhasgfjhk324234asdjhkguw';
-
-const app = express();
 
 app.use(cors({credentials:true,origin:'https://blog-app-frontend-lac.vercel.app'}));
 app.use(express.json());
@@ -34,95 +33,100 @@ mongoose.connect('mongodb+srv://admin:jcG4Nse0pRo8BDVR@cluster0.8tvggus.mongodb.
 
 
 
-app.post('/register', async (req, res) => {
-  const { username, password } = req.body;
-  try {
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+
+
+
+app.post('/register', async (req,res) => {
+  const {username,password} = req.body;
+  try{
     const userDoc = await User.create({
       username,
-      password: bcrypt.hashSync(password, salt),
+      password:bcrypt.hashSync(password,salt),
     });
     res.json(userDoc);
-  } catch (e) {
+  } catch(e) {
     console.log(e);
     res.status(400).json(e);
   }
 });
 
-app.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-  const userDoc = await User.findOne({ username: username });
+app.post('/login', async (req,res) => {
+  const {username,password} = req.body;
+  const userDoc = await User.findOne({username});
   const passOk = bcrypt.compareSync(password, userDoc.password);
   if (passOk) {
-    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+    // logged in
+    jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
       if (err) throw err;
       res.cookie('token', token).json({
-        id: userDoc._id,
+        id:userDoc._id,
         username,
       });
     });
   } else {
-    res.status(400).json('Wrong Credentials');
+    res.status(400).json('wrong credentials');
   }
 });
 
-app.get('/profile', (req, res) => {
-  const { token } = req.cookies;
-  jwt.verify(token, secret, {}, (err, info) => {
-    if (err) {
-      res.status(403).json({ error: 'Invalid token' });
-    } else {
-      res.json(info);
-    }
+app.get('/profile', (req,res) => {
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, (err,info) => {
+    if (err) throw err;
+    res.json(info);
   });
 });
 
-app.post('/logout', (req, res) => {
+app.post('/logout', (req,res) => {
   res.cookie('token', '').json('ok');
 });
 
-app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
-  const { originalname, path } = req.file;
+app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
+  const {originalname,path} = req.file;
   const parts = originalname.split('.');
   const ext = parts[parts.length - 1];
-  const newPath = path + '.' + ext;
+  const newPath = path+'.'+ext;
   fs.renameSync(path, newPath);
 
-  const { token } = req.cookies;
-  jwt.verify(token, secret, {}, async (err, info) => {
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, async (err,info) => {
     if (err) throw err;
-    const { title, summary, content } = req.body;
+    const {title,summary,content} = req.body;
     const postDoc = await Post.create({
       title,
       summary,
       content,
-      cover: newPath,
-      author: info.id,
+      cover:newPath,
+      author:info.id,
     });
     res.json(postDoc);
   });
+
 });
 
-app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
+app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
   let newPath = null;
   if (req.file) {
-    const { originalname, path } = req.file;
+    const {originalname,path} = req.file;
     const parts = originalname.split('.');
     const ext = parts[parts.length - 1];
-    newPath = path + '.' + ext;
+    newPath = path+'.'+ext;
     fs.renameSync(path, newPath);
   }
 
-  const { token } = req.cookies;
-  jwt.verify(token, secret, {}, async (err, info) => {
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, async (err,info) => {
     if (err) throw err;
-    const { id, title, summary, content } = req.body;
+    const {id,title,summary,content} = req.body;
     const postDoc = await Post.findById(id);
     const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
     if (!isAuthor) {
-      return res.status(400).json('You are not the author of this post.');
+      return res.status(400).json('you are not the author');
     }
-
-    await postDoc.updateOne({
+    await postDoc.update({
       title,
       summary,
       content,
@@ -131,31 +135,36 @@ app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
 
     res.json(postDoc);
   });
+
 });
 
-app.get('/post', async (req, res) => {
-  res.json(await Post.find()
-    .populate('author', ['username'])
-    .sort({ createdAt: -1 })
-    .limit(20)
+app.get('/post', async (req,res) => {
+  res.json(
+    await Post.find()
+      .populate('author', ['username'])
+      .sort({createdAt: -1})
+      .limit(20)
   );
 });
 
 app.get('/post/:id', async (req, res) => {
-  const { id } = req.params;
+  const {id} = req.params;
   const postDoc = await Post.findById(id).populate('author', ['username']);
   res.json(postDoc);
-});
+})
 
-// Serve static files from the React app
+
+
+
+
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Catch-all handler to serve React's index.html for any route not handled by your API
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
-
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(4000);
+// yes
+//jcG4Nse0pRo8BDVR
+//...
+app.listen(process.env.PORT || 4000);
