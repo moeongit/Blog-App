63abba5cb7e77dbbb0eaaccad017908f1c38638b
moeongit <malrobiay04@gmail.com>
2024-07-27 04:30:48 -0600

    REVERTING

--- api/index.js
+++ api/index.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
@@ -10,9 +8,7 @@ const app = express();
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const multer = require('multer');
-const multerS3 = require('multer-s3');
-const AWS = require('aws-sdk');
-// const uploadMiddleware = multer({ dest: 'uploads/' });
+const uploadMiddleware = multer({ dest: 'uploads/' });
 const fs = require('fs');
 
 const salt = bcrypt.genSaltSync(10);
@@ -21,24 +17,8 @@ const secret = 'asdjasjfkgjkhasgfjhk324234asdjhkguw';
 app.use(cors({credentials:true,origin:'https://blog-app-frontend-lac.vercel.app'}));
 app.use(express.json());
 app.use(cookieParser());
-// app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION
-});
-
-const uploadS3 = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: process.env.S3_BUCKET_NAME,
-    acl: 'public-read',
-    key: function (req, file, cb) {
-      cb(null, Date.now().toString() + '-' + file.originalname)
-    }
-  })
-});
 
 
 mongoose.connect('mongodb+srv://admin:jcG4Nse0pRo8BDVR@cluster0.8tvggus.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
@@ -97,7 +77,13 @@ app.post('/logout', (req,res) => {
   res.cookie('token', '').json('ok');
 });
 
-app.post('/post', uploadS3.single('file'), async (req,res) => {
+app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
+  const {originalname,path} = req.file;
+  const parts = originalname.split('.');
+  const ext = parts[parts.length - 1];
+  const newPath = path+'.'+ext;
+  fs.renameSync(path, newPath);
+
   const {token} = req.cookies;
   jwt.verify(token, secret, {}, async (err,info) => {
     if (err) throw err;
@@ -106,18 +92,22 @@ app.post('/post', uploadS3.single('file'), async (req,res) => {
       title,
       summary,
       content,
-      cover: req.file.location, // S3 file URL
+      cover:newPath,
       author:info.id,
     });
     res.json(postDoc);
   });
+
 });
 
-
-app.put('/post', uploadS3.single('file'), async (req,res) => {
+app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
   let newPath = null;
   if (req.file) {
-    newPath = req.file.location;
+    const {originalname,path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    newPath = path+'.'+ext;
+    fs.renameSync(path, newPath);
   }
 
   const {token} = req.cookies;
@@ -138,6 +128,7 @@ app.put('/post', uploadS3.single('file'), async (req,res) => {
 
     res.json(postDoc);
   });
+
 });
 
 app.get('/post', async (req,res) => {
